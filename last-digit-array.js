bbbf8405a28cf1fb7e631a9e265ec4d4dbb7334b
George Seeger <george@polylytics.com>
2017-11-22 08:56:33 +0000

    623 tests pass, one fails

--- last-digit-array.js
+++ last-digit-array.js
@@ -1,12 +1,13 @@
 var lastDigit = function(arr) {
   if (arr.length === 0) return 1;
   if (arr.length === 1) return arr[0] % 10;
+  console.log(arr);
   if (arr.length === 2) return lastTwoDigits(arr[0].toString(), arr[1].toString()) % 10;
   var reduceWith = function(twoDigits) {
-    return lastDigit(arr.slice(0, arr.length - 2).concat([twoDigits.toString().slice(-2)]));
+    return lastDigit(arr.slice(0, arr.length - 2).concat([twoDigits]));
   }
     
-  var base = arr.slice(-2, -1).toString().slice(-2).padStart(2, "0");
+  var base = arr.slice(-2, -1).toString().slice(-2);
   var expo = arr.slice(-1).toString().slice(-2);
   return reduceWith(lastTwoDigits(base, expo));
 }
@@ -14,8 +15,10 @@ var lastDigit = function(arr) {
 var lastTwoDigits = function(base, expo) {
   base = base.toString();
   expo = expo.toString();
-  if (expo.length == 1) return (base.slice(-2) ** expo).toString().slice(-2);
-  
+  if (expo.length == 1 && base.length == 1) return (base.slice(-2) ** expo).toString().slice(-2);
+  if (base === "0") return "0";
+  if (base === "1") return "1";  
+
   function endsInOne(base, expo) {
     return (base.toString().slice(-2, -1) * (expo % 10) + "1").toString().slice(-2);
   }
@@ -24,12 +27,13 @@ var lastTwoDigits = function(base, expo) {
     var rem = base / 2;
     var oddPowerofXX24 = parseInt(expo / 10) % 2 == 1;
     var ans = (2 ** (expo % 10)) * (oddPowerofXX24 ? 24 : 76) * lastTwoDigits(rem, expo);
-    return ans.toString().slice(-2);
+    //console.log(ans);
+    return ans.toString().slice(-2).padStart(2, "0");
   }
 
   switch (base.slice(-1)) {
     case "0":
-      return (10 ** expo.slice(-1)).toString().slice(-2);
+      return "00";
     case "1":
       return endsInOne(base, expo);
     case "5":
@@ -37,10 +41,10 @@ var lastTwoDigits = function(base, expo) {
                     ? "75"
                     : "25";
     case "9":
-      return (base ** (expo % 2)) * endsInOne(base ** 2, parseInt(expo / 2));
+      return ((base ** (expo % 2)) * endsInOne(base ** 2, parseInt(expo / 2))).toString().slice(-2);
     case "3":
     case "7":
-      return (base ** (expo % 4)) * endsInOne(base ** 4, parseInt(expo / 4));
+      return ((base ** (expo % 4)) * endsInOne(base ** 4, parseInt(expo / 4))).toString().slice(-2);
     case "2":
     case "4":
     case "6":
@@ -58,14 +62,19 @@ console.log(lastTwoDigits(19, 266)); //81
 console.log(lastTwoDigits(71, 56747)); //91
 console.log(lastTwoDigits(125, 28)); //25
 console.log(lastTwoDigits(115, 35)); //75
-console.log(lastTwoDigits(2, 0)); //01
-console.log(lastTwoDigits(2, 1)); //02
-console.log(lastTwoDigits(2, 2)); //04
+console.log(lastTwoDigits(2, 0)); //1
+console.log(lastTwoDigits(2, 1)); //2
+console.log(lastTwoDigits(2, 2)); //4
 console.log(lastTwoDigits(2, 4)); //16
-console.log(lastTwoDigits(101, 2)); //02
+console.log(lastTwoDigits(101, 2)); //01
 console.log(lastTwoDigits(2, 201)); //52
 console.log(lastTwoDigits(2, "01")); //52
 console.log(lastTwoDigits(12, "10")); //24
+console.log(lastTwoDigits(47, "23")); //23
+console.log(lastTwoDigits(390557, "76")); //01
+
+console.log(lastDigit([ 761983,  64283,  787971,  469635,  130031,  390557,  272118,  683154,  426950 ])); //7
+console.log(lastDigit([ 2147483647, 2147483647, 2147483647, 2147483647 ])); //9
 
 
 
