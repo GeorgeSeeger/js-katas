e764cb06aadba4e162d49e487aaf87d756c1b2bd
George Seeger <george@polylytics.com>
2018-01-11 09:06:06 +0000

    wew that was really really reeeally annoying

--- functional-lists.js
+++ functional-lists.js
@@ -7,10 +7,9 @@ EmptyList.prototype.constructor = EmptyList;
 EmptyList.prototype.toString = function() { return "()"; };
 EmptyList.prototype.isEmpty = function() { return true; };
 EmptyList.prototype.length = function() { return 0; };
-EmptyList.prototype.push = function(x) { return new ListNode(x, new EmptyList()); };
+EmptyList.prototype.push = function(x) { return new ListNode(x, this); };
 EmptyList.prototype.remove = function(x) { return this; };
 EmptyList.prototype.append = function(xs) { return xs; };
-EmptyList.prototype.valString = function() { return ""; };
 
 function ListNode(value, next) { this.value = value; this.next = next; }
 ListNode.prototype = new List();
@@ -25,11 +24,10 @@ ListNode.prototype.tail = function() { return this.next;  };
 ListNode.prototype.length = function() { return 1 + this.next.length(); };
 ListNode.prototype.push = function(x) { return new ListNode(x, this) };
 ListNode.prototype.remove = function(x) {
-    var node = this;  
-    if (node.head() === x) { return node.tail()we }
-    if (!node.tail().isEmpty() && node.tail().head() === x) { return new ListNode(node.head(), node.tail().tail().remove(x)) ;}
+    if (this.head() === x) { return this.tail().remove(x); }
+    if (!this.tail().isEmpty() && this.tail().head() == x) { return new ListNode(this.head(), this.tail().tail().remove(x)) ;}
     var nextNode = this.next.remove(x);
-    return nextNode == this.next ? this : new ListNode(this.value, nextNode);
+    return nextNode === this.next ? this : new ListNode(this.value, nextNode);
 };
 ListNode.prototype.append = function(xs) {
   if (this.next.isEmpty()) {return new ListNode(this.value, xs);}
