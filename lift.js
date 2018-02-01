706e4ea60393798889d1ec89bff1f6d7430a31cc
George Seeger <george@polylytics.com>
2018-02-01 08:45:14 +0000

    the lift works, but the algorithm is just too slow :(

--- lift.js
+++ lift.js
@@ -2,27 +2,31 @@ var theLift = function(queues, capacity) {
     var floorsVisited = [ 0 ];
     var lift = new Lift(queues, capacity);
     while (lift.queues.some(function(q) { return q.length > 0; }) || lift.holding.length != 0) {
+        if (lift.floor === 0) lift.dir = 'up';
+        if (lift.floor === lift.maxFloor) lift.dir = 'down';
         if (lift.isEmpty()) {
             var floor = lift.findClosestInSameDirection();
-            if (!floor) { 
+            if (floor === undefined) { 
                 floor = lift.findFurthestInOppositeDirection();
-                lift.dir = lift.dirs.find(function(d) { return d !== lift.dir; })
-                if (!floor) return floorsVisited;
+                lift.switchDirection();
             }
-            lift.floor = floor;
-            floorsVisited.push(floor);
-            lift.stopAtFloor();
+            if (floor !== undefined) {
+                if (lift.floor !== floor) { floorsVisited.push(floor); }
+                lift.floor = floor;
+                lift.stopAtFloor();
+            }
         } else {
             lift.floor += lift.dir === 'up' ? 1 : -1;
-            if (lift.holding.some(function(p) { return p.destination == lift.floor; }) || lift.queues[lift.floor].some(function(p) { return p.location == lift.floor; })) {
+            if (lift.holding.some(function(p) { return p.destination == lift.floor; }) || (!lift.isFull() && lift.queues[lift.floor].some(function(p) { return p.location == lift.floor; }))) {
                 floorsVisited.push(lift.floor);
             }
             lift.stopAtFloor();
         }
     }
-    floorsVisited.push(0);
+    if (lift.floor != 0) floorsVisited.push(0);
     return floorsVisited;
-  }
+}
+
 function Person(location, destination) {
     this.location = location;
     this.destination = destination;
@@ -42,6 +46,7 @@ function Lift(queues, capacity) {
 
 Lift.prototype.constructor = Lift;
 Lift.prototype.isEmpty = function() { return this.holding.length === 0; }
+Lift.prototype.isFull = function() { return this.holding.length === this.capacity; }
 Lift.prototype.findClosestInSameDirection = function() {
     for (var i = this.floor; i >= 0 && i <= this.maxFloor; this.dir === 'up' ? i++: i--) {
         if (this.queues[i].length > 0 && this.queues[i].some(function(p) { return p.dir === this.dir; }, this)) {
@@ -59,31 +64,16 @@ Lift.prototype.findFurthestInOppositeDirection = function() {
 Lift.prototype.stopAtFloor = function() {
     var queue = this.queues[this.floor];
     this.holding = this.holding.filter(function(p) { return p.destination !== this.floor; }, this);
+    if (this.isEmpty() && !this.findClosestInSameDirection()) {
+        this.switchDirection();
+    }
     for(var i = 0; i < queue.length; i++){
-        if (this.holding.length < this.capacity && queue[i].dir === this.dir) {
+        if (!this.isFull() && queue[i].dir === this.dir) {
             this.holding.push(queue[i]);
             queue[i] = null;
         }
     }
     this.queues[this.floor] = queue.filter(function(p) { return p; });    
 }
-
-theLift([
-    [], // G
-    [], // 1
-    [5,5,5], // 2
-    [], // 3
-    [], // 4
-    [], // 5
-    [], // 6
-  ], 5);
-
-theLift([
-    [], // G
-    [0], // 1
-    [], // 2
-    [], // 3
-    [2], // 4
-    [3], // 5
-    [], // 6
-  ], 5)
+Lift.prototype.switchDirection = function() { this.dir = this.dirs.find(function(d) { return d !== this.dir; }, this); }
+theLift([ [], [ 2 ], [ 3, 3, 3 ], [ 1 ], [], [], [] ], 1)
